0ba56ec44cb01fe69f87447305efb2a8abc1567e
AxAnder0095 <doriscafe@hotmail.com>
2026-02-10 22:49:51 -0600

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import { Navbar } from "../components/NavBar.jsx";
 import '../styles/Home.scss'
 import myImage from '../imgs/thumb2.jpg'
 import { Link } from "react-router-dom";
+import { RandomReveal } from "react-random-reveal";
+import { useState } from "react";
 
 export const Home = () => {
+    const [key, setKey] = useState(0);
     return (
         <div className="home-container">
             <header>
@@ -12,7 +15,21 @@ export const Home = () => {
             <main className="main">
                 <section className="information">
                     <article className="about">
-                        <p className="intro-title">INTRODUCTION</p>
+                        <p className="intro-title">
+                            <RandomReveal
+                                key={key}
+                                isPlaying={true}
+                                duration={2}
+                                characters={"INTRODUCTION"}
+                                characterSet={['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']}
+                                updateInterval={0.05}
+                                onComplete={() => {
+                                    setTimeout(() => {
+                                        setKey(prev => prev + 1);
+                                    }, 10000);
+                                }}
+                            />
+                        </p>
                         <div className="intro-wrapper">
                             <div className="intro-img">
                                 <img src={myImage} alt="profile" className="img-me" />
